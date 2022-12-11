bfa989a593008e9e28e8d073690f7716a30b5b31
Akash <phalke.aka@gmail.com>
2022-12-11 12:16:07 +0530

    update

---
+++ terraform-basic/ec2.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "web" {
+  ami           = ami-0c2254f22b03751fa
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+output "private_dns" {
+ value = aws_instance.web.private_dns   
+}
